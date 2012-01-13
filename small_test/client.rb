4518a6fc550a9d1f66350c5606d0fd1c8b37d75a
Gibheer <gibheer@gmail.com>
2012-01-13 21:33:00 +0100

    check for eof

--- small_test/client.rb
+++ small_test/client.rb
@@ -10,7 +10,7 @@ publisher = context.socket(ZMQ::PUB)
 publisher.connect('tcp://127.0.0.1:9999')
 puts "beginning with the sends"
 
-while true
+until $stdin.eof? do
   s = $stdin.readline
   if s =~ /^\s[0-9].*/
     publisher.send_string(s)
