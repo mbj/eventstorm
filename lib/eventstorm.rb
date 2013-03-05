e95fd2508cf2ce5f03da41e98a0b83b6452bdfc7
Markus Schirp <mbj@seonic.net>
2013-03-05 15:12:47 +0100

    Nuke all code and specs

--- lib/eventstorm.rb
+++ lib/eventstorm.rb
@@ -1,37 +1,4 @@
-require 'time'
-require 'bson'
-require 'ffi-rzmq'
 
-require 'eventstorm/client'
-
+# Root namespace
 module Eventstorm
-  # Access global zmq_context
-  #
-  # @return [ZMQ::Context]
-  #
-  # @api public
-  def self.zmq_context
-    @zmq_context ||= ZMQ::Context.new(1)
-  end
-
-  def self.setup(target)
-    if @client
-      raise 'Eventstorm.setup was already called'
-    end
-
-    @client = Client.new(target)
-
-    self
-  end
-
-  def self.client
-    if @client
-      return @client
-    end
-    raise 'Eventstorm was not setup, call Eventstorm.setup(target)'
-  end
-
-  def self.fire
-    @client.fire
-  end
 end
